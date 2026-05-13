671879359ce10a61d147c11e1f2860f2052f0708
Renor Oliveira Guimarães <renornew@gmail.com>
2026-05-13 18:14:13 -0300

    mudança de while  para for

--- main.js
+++ main.js
@@ -5,10 +5,9 @@ function tocaSom(idElementoAudio){
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
 
 // while
-while(contador < listaDeTeclas.length) {
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
@@ -20,10 +19,6 @@ while(contador < listaDeTeclas.length) {
     tecla.onclick = function () {
         tocaSom(idAudio);
     }
-
-   
-    contador = contador + 1;
-
   
 }
 
